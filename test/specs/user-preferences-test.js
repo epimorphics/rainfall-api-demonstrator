43d5161987769bfc6a413b80d25999af162a332c
Ian Dickinson <i.j.dickinson@gmail.com>
2016-09-19 21:04:20 +0100

    Add form fields for other filters

--- test/specs/user-preferences-test.js
+++ test/specs/user-preferences-test.js
@@ -11,7 +11,37 @@ describe("user preferences", () => {
 
   it("sets the river name if given", () => {
     browser.url("/user-preferences-fixture.html?riverName=Brue");
-    const riverName = browser.element( "#riverName").getValue();
-    assert( riverName == "Brue" );
+    const name = browser.element( "#riverName").getValue();
+    assert( name == "Brue" );
+  });
+
+  it("sets the catchment name if given", () => {
+    browser.url("/user-preferences-fixture.html?catchmentName=Brue");
+    const name = browser.element( "#catchmentName").getValue();
+    assert( name == "Brue" );
+  });
+
+  it("sets the station name if given", () => {
+    browser.url("/user-preferences-fixture.html?stationName=Brue");
+    const name = browser.element( "#stationName").getValue();
+    assert( name == "Brue" );
+  });
+
+  it("sets the latitude if given", () => {
+    browser.url("/user-preferences-fixture.html?lat=1234");
+    const name = browser.element( "#lat").getValue();
+    assert( name == "1234" );
+  });
+
+  it("sets the longitude if given", () => {
+    browser.url("/user-preferences-fixture.html?long=1234");
+    const name = browser.element( "#long").getValue();
+    assert( name == "1234" );
+  });
+
+  it("sets the radius if given", () => {
+    browser.url("/user-preferences-fixture.html?dist=1234.5");
+    const name = browser.element( "#dist").getValue();
+    assert( name == "1234.5" );
   });
 });
